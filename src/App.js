85f36a8e6fbfa487a84713a1ebcb8c759c6a4736
Ric Mershon <rmershon@tsys.com>
2021-12-07 16:24:57 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Information from './info-json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [search, setSearch] = useState(null);
+
+    const styleInfo = {
+        paddingRight:'10px'
+    };
+
+    const elementStyle ={
+        border:'solid',
+        borderRadius:'10px',
+        position:'relative',
+        left:'10vh',
+        height:'3vh',
+        width:'20vh',
+        marginTop:'5vh',
+        marginBottom:'10vh'
+    };
+
+    const filtered = Information.filter((data) => {
+        if (search == null) {
+            return data
+        }
+        return (
+            data.name.toLowerCase().includes(search.toLowerCase()) ||
+            data.country.toLowerCase().includes(search.toLowerCase()))
+    });
+
+    const items = filtered.map((data, index) => (
+        <>
+            <ul>
+                <li key={index} style={{position:'relative',left:'10vh'}}>
+                    <span style={styleInfo}>{data.name}</span>
+                    <span style={styleInfo}>{data.age}</span>
+                    <span style={styleInfo}>{data.country}</span>
+                </li>
+            </ul>
+        </>
+    ));
+
+    return (
+        <>
+            <input
+                type="text"
+                placeholder="Enter item to be searched"
+                style={elementStyle}
+                onChange={(event) => setSearch(event.target.value)}
+            />
+            {items}
+        </>
+    )
 }
 
 export default App;
